d5b0be7ef365b2f96b079c3e78ed3b00bcd81032
ViktoriaBashinska <158279310+ViktoriaBashinska@users.noreply.github.com>
2024-04-22 19:47:37 +0300

    додаємо фото автора, кнопки та перехід при натисканні

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log(1)
+document.getElementById("document-btn").addEventListener("click", function() {
+    window.open("https://docs.google.com/document/d/1VakpU3LTmTrxoBdm2koGrIk3R2XVvExueMmLKw-_DjM/edit", "_blank");
+});
 
+document.getElementById("feedback-btn").addEventListener("click", function() {
+    window.open("https://forms.gle/4Hpr5WDodHn5e1US7", "_blank");
+});
+
+document.getElementById("game-btn").addEventListener("click", function() {
+    window.open("https://viktoriabashinska.github.io/Phaser2stGame/", "_blank");
+});
