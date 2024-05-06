05867b757a2f765d6f352ced6f8943d675120c13
ViktoriaBashinska <158279310+ViktoriaBashinska@users.noreply.github.com>
2024-05-06 23:06:46 +0300

    працюємо над оформленням сайту

--- market.js
+++ market.js
@@ -1,21 +1,21 @@
 //перевірка підключеного файлу скрптів
-console.log('Перевірка підключеного файлу скриптів market.js')
+//console.log('Перевірка підключеного файлу скриптів market.js')
 
 //отримання елементу з ідентифікатором items
-let itemsDiv = document.getElementById("items");
+//let itemsDiv = document.getElementById("items");
 
 //пеервірка існування знайденого блоку
-if (itemsDiv) {
+//if (itemsDiv) {
     //вивід знайденого елементу
-    console.log(itemsDiv)
+ //   console.log(itemsDiv)
     //вивід значення поля знайденого елементу
-    console.log('Поле classlist: ', itemsDiv.classList)
-    console.log('Поле id:', itemsDiv.id)
-    console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+   // console.log('Поле classlist: ', itemsDiv.classList)
+  //  console.log('Поле id:', itemsDiv.id)
+    //console.log('Поле clientWidth: ', itemsDiv.clientWidth)
+ //   console.log('Поле innerHTML: ', itemsDiv.innerHTML)
     //додавання відформатованого HTML коду в блок
-    itemsDiv.innerText = 'Програмно доданий текст'
-} else {
+ //   itemsDiv.innerText = 'Програмно доданий текст'
+//} else {
     //вивід повідомлення про не знайдений елемент
-    console.log('Блок товарів не знайдено')
-}
+  //  console.log('Блок товарів не знайдено')
+//}
