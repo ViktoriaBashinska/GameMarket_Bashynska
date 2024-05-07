bb8b2a52b4e8296ca5c61d861a72c4dae074454f
ViktoriaBashinska <158279310+ViktoriaBashinska@users.noreply.github.com>
2024-05-08 00:12:34 +0300

    опрацьовуємо, додали 100 елементів

--- market.js
+++ market.js
@@ -1,21 +1,31 @@
-//перевірка підключеного файлу скрптів
-//console.log('Перевірка підключеного файлу скриптів market.js')
+//перевірка підключеного файлу скриптів
+console.log('Перевірка підключеного файлу скриптів')
 
 //отримання елементу з ідентифікатором items
-//let itemsDiv = document.getElementById("items");
+let itemsDiv = document.getElementById("items");
 
-//пеервірка існування знайденого блоку
-//if (itemsDiv) {
-    //вивід знайденого елементу
- //   console.log(itemsDiv)
-    //вивід значення поля знайденого елементу
-   // console.log('Поле classlist: ', itemsDiv.classList)
-  //  console.log('Поле id:', itemsDiv.id)
-    //console.log('Поле clientWidth: ', itemsDiv.clientWidth)
- //   console.log('Поле innerHTML: ', itemsDiv.innerHTML)
-    //додавання відформатованого HTML коду в блок
- //   itemsDiv.innerText = 'Програмно доданий текст'
-//} else {
-    //вивід повідомлення про не знайдений елемент
-  //  console.log('Блок товарів не знайдено')
-//}
+ //перевірка існування знайденого блоку
+if (itemsDiv) {
+   //вивід знайденого елементу
+   console.log (itemsDiv)
+   //вивід значення поля знайденого елементу
+   console.log ('Поле classList:', itemsDiv.classList)
+   console.log ('Поле id:', itemsDiv.id)
+   console.log ('Поле clientWidth:', itemsDiv.clientWidth)
+   console.log ('Поле innerHTML:', itemsDiv.innerHTML)
+
+   //додавання тексту в блок
+   //.innerText = 'Перший програмно доданий текст'
+   //itemsDiv.innerText += 'Другий програмно доданий текст'
+   //додавання відформатованого HTML коду в блок
+   //itemsDiv.innerHTML += '<div class = "item"></div>'
+   //itemsDiv.innerHTML += '<div class = "item"></div>'
+   //itemsDiv.innerHTML += '<div class = "item"></div>'
+   //itemsDiv.innerHTML += '<div class = "item"></div>'
+   for (let i = 0; i < 100; i++) {
+      itemsDiv.innerHTML += '<div class = "item"></div>'
+   }
+} else {
+//вивід повідомлення про незнайдений блок
+console.log ('Блок товарів не знайдено')
+}
