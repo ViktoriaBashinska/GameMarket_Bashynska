cfd400cfd28f03f9a6bd0ea6bf9b71c5c8974b10
ViktoriaBashinska <158279310+ViktoriaBashinska@users.noreply.github.com>
2024-05-12 23:02:48 +0300

    Update market.js

--- market.js
+++ market.js
@@ -2,17 +2,17 @@
 console.log('Перевірка підключеного файлу скриптів')
 
 //отримання елементу з ідентифікатором items
-let itemsDiv = document.getElementById("items");
+//let itemsDiv = document.getElementById("items");
 
  //перевірка існування знайденого блоку
-if (itemsDiv) {
+//if (itemsDiv) {
    //вивід знайденого елементу
-   console.log (itemsDiv)
+   //console.log (itemsDiv)
    //вивід значення поля знайденого елементу
-   console.log ('Поле classList:', itemsDiv.classList)
-   console.log ('Поле id:', itemsDiv.id)
-   console.log ('Поле clientWidth:', itemsDiv.clientWidth)
-   console.log ('Поле innerHTML:', itemsDiv.innerHTML)
+  // console.log ('Поле classList:', itemsDiv.classList)
+   //console.log ('Поле id:', itemsDiv.id)
+   //console.log ('Поле clientWidth:', itemsDiv.clientWidth)
+  // console.log ('Поле innerHTML:', itemsDiv.innerHTML)
 
    //додавання тексту в блок
    //.innerText = 'Перший програмно доданий текст'
@@ -22,10 +22,46 @@ if (itemsDiv) {
    //itemsDiv.innerHTML += '<div class = "item"></div>'
    //itemsDiv.innerHTML += '<div class = "item"></div>'
    //itemsDiv.innerHTML += '<div class = "item"></div>'
-   for (let i = 0; i < 100; i++) {
-      itemsDiv.innerHTML += '<div class = "item"></div>'
-   }
-} else {
+   //for (let i = 0; i < 6; i++) {
+   //  itemsDiv.innerHTML += '<div class = "item"></div>'
+  // }
+//} else {
 //вивід повідомлення про незнайдений блок
-console.log ('Блок товарів не знайдено')
+//console.log ('Блок товарів не знайдено')
+//}
+
+//виведення масиву товарів
+let itemsArray = [
+   'Газонокосарка 43',
+   'Електричний тример 110',
+   'Електрична газонокосарка 32',
+   'Акумуляторний оприскувач 12 N',
+]
+
+//отримання елементу з ідентифікатором items
+let itemsDiv = document.getElementById("items");
+
+//перевірка існування знайденого блоку
+if (itemsDiv) {
+   //виведення елементів масиву
+   itemsArray.forEach((item) =>{
+      //console.log(item)
+      //itemsDiv.innerText += item
+      //спеціальний апостроф (англ розкладка, біля кнопки 1)
+      itemsDiv.innerHTML += 
+      `
+      <div class = "item">
+      <p>${item}</p>
+      </div>
+      `
+   })
+} else {
+   //вивід повідомлення про не знайдений блок
+   console.log('Блок товарів не знайдено')
+}
+//виведення масиву в консоль
+console.log (itemsArray)
+//виведення в консоль елементів масиву
+for (let i = 0; i < itemsArray.length; i++) {
+   console.log (itemsArray[i])
 }
