68622b1ab58f7060a3f21be9cd7ee535aed1ce24
ViktoriaBashinska <158279310+ViktoriaBashinska@users.noreply.github.com>
2024-05-21 23:25:33 +0300

    додали масив товарів

--- market.js
+++ market.js
@@ -1,107 +1,189 @@
 //перевірка підключеного файлу скриптів
-console.log('Перевірка підключеного файлу скриптів')
-//отримання елементу з ідентифікатором items
-//let itemsDiv = document.getElementById("items");
- //перевірка існування знайденого блоку
-//if (itemsDiv) {
-   //вивід знайденого елементу
-   //console.log (itemsDiv)
-   //вивід значення поля знайденого елементу
-  // console.log ('Поле classList:', itemsDiv.classList)
-   //console.log ('Поле id:', itemsDiv.id)
-   //console.log ('Поле clientWidth:', itemsDiv.clientWidth)
-  // console.log ('Поле innerHTML:', itemsDiv.innerHTML)
+//console.log('Перевірка підключеного файлу скрипту market.js')
 
-   //додавання тексту в блок
-   //.innerText = 'Перший програмно доданий текст'
-   //itemsDiv.innerText += 'Другий програмно доданий текст'
-   //додавання відформатованого HTML коду в блок
-   //itemsDiv.innerHTML += '<div class = "item"></div>'
-   //itemsDiv.innerHTML += '<div class = "item"></div>'
-   //itemsDiv.innerHTML += '<div class = "item"></div>'
-   //itemsDiv.innerHTML += '<div class = "item"></div>'
-   //for (let i = 0; i < 6; i++) {
-   //  itemsDiv.innerHTML += '<div class = "item"></div>'
-  // }
-//} else {
-//вивід повідомлення про незнайдений блок
-//console.log ('Блок товарів не знайдено')
-//}
-//виведення масиву товарів
-//let itemsArray = [
-  // 'Газонокосарка 43',
-   //'Електричний тример 110',
-   //'Електрична газонокосарка 32',
-   //'Акумуляторний оприскувач 12 N',
-//]
-//отримання елементу з ідентифікатором items
-//let itemsDiv = document.getElementById("items");
-//перевірка існування знайденого блоку
-//if (itemsDiv) {
-   //виведення елементів масиву
-  // itemsArray.forEach((item) =>{
-      //console.log(item)
-      //itemsDiv.innerText += item
-      //спеціальний апостроф (англ розкладка, біля кнопки 1)
-      //itemsDiv.innerHTML += `
-      //<div class = "item">
-      //<p>${item}</p>
-      //</div>`
-   //})
-//} else {
-   //вивід повідомлення про не знайдений блок
-  // console.log('Блок товарів не знайдено')
-//}
-//виведення масиву в консоль
-//console.log (itemsArray)
-//виведення в консоль елементів масиву
-//for (let i = 0; i < itemsArray.length; i++) {
-//   console.log (itemsArray[i])
-//}
+//Отримання елементу з ідентифікатором items
+//let itemsDiv = document.getElementById("items")
 
-//визначення масиву об'єктів
+//визначення масиву товарів 
+// let itemsArray = [
+//     'Мотокоса 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N'
+// ]
+
 let itemsArray = [
    {
-      firstName: "Віталій",
-      lastName: "Шатківський",
-      age: 43,
-      subject: "CS",
-      photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
-      url: "https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
+       title:"Мотокоса 43",
+       image:'img/1.webp',
+       mono: 4,
+       pb: 8,
+       prise_standart: 5499,
+       prise_discount:4497,
+       prise_coupon:4395,
    },
    {
-      firstName: "Наталія",
-      lastName: "Венцель",
-      age: 18,
-      subject: "Адміністратор",
-      photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg",
-      url: "https://lyceum.ztu.edu.ua/team/ventsel-n-v/",
-   }
+       title:"Електричний тример 110",
+       image:'img/2.webp',
+       mono: 5,
+       pb: 10,
+       prise_standart:4497,
+       prise_discount:3498,
+       prise_coupon:3396,
+   },
+   {
+       title:"Електрична газонокосарка 32",
+       image:'img/3.webp',
+       mono: 8,
+       pb: 11,
+       prise_standart:4497,
+       prise_discount:3297,
+       prise_coupon:3185,
+   },
+   {
+       title:"Акумуляторний оприскувач 12 N",
+       image:'img/4.webp',
+       mono: 6,
+       pb: 12,
+       prise_standart:2190,
+       prise_discount:1698,
+       prise_coupon:1596,
+   },
+//    {
+//     firstName:"Віталій",
+//     lastName:"",
+//     age: 18,
+//     subject: "CS",
+//     photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
+//     url: "https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/", 
+//    }, 
+//    {
+//     firstName: "Наталія",
+//     lastName: "Венцель",
+//     age: 40,
+//     subject:"Директорка",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/ventsel-n-v/",
+//    },
+//    {
+//     firstName: "Олена",
+//     lastName: "Геча",
+//     age:18,
+//     subject:"Історія",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/hecha-olena-anatoliyivna-683x1024.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/hecha-o-a/",
+//    },
+//    {
+//     firstName: "Вікторія",
+//     lastName: "Нелипович",
+//     age: 18,
+//     subject:"Математика",
+//     photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/nelypovych-viktoriya-vitaliyivna-683x1024.jpg",
+//     url:"https://lyceum.ztu.edu.ua/team/nelypovych-viktoriya-vitaliyivna/",
+//    },
+
 ]
-//отримання елементу з ідентифікатором item
-let itemsDiv = document.getElementById("items");
-//перевірка існування знайденого блоку
+
+//отримання елементу з індифікатором items
+let itemsDiv  = document.getElementById("items");
+
+//checing on 
 if (itemsDiv) {
-   //створення блоків по кількості елементів масиву
+   //creating blocks by quantity of elements
    itemsArray
-   //сортування за прізвищем
+   //sorting by lastnames
    //.sort((a,b) => a.lastName.localeCompare(b.lastName))
-   .forEach ((item, index) => {
-      //виводимо на веб сторінку елемент масиву в блок з класом item
-      itemsDiv.innerHTML += 
-   `
-   <div class = "item">
-      <h2>Вчитель № ${index + 1} з ${itemsArray.length}</h2>
-      <p>${item.firstName} ${item.lastName}</p>
-      <p>Предмет: ${item.subject}</p>
-      <p><img src = "${item.photo}" class="item-image"></p>
-      <p><a href = "${item.url}" target = "_blank" class = "bonus price">Профіль<a/></p>
-      <p><a href = "mailto:${item.email}?subject=Питання&body=Доброго дня" target = "_blank"
-      class = "price "> Написати </a></p>
-   </div>
-   `
-   })
-} else {
-   //вивід повідомлення про не знайдений блок
+   //
+       .forEach((item,index) => (
+           //виводимо 
+           itemsDiv.innerHTML +=
+           `
+           <div class="item">
+               <div class="item-title">${item.title}</div>
+               <div class="item-image">
+                   <img src="${item.image}" alt="${item.name}">
+               </div>
+               <div class="parts-pay">
+                   <div><img src="img/monolapka.png" alt="">${item.mono}</div>
+                   <div><img src="img/pb.jpg" alt="">${item.pb}</div>
+               </div>
+               <div class="price">
+                   <div><span>${item.prise_standart}</span><sup>грн</sup></div>
+                   <div><span>${item.prise_discount}</span><sup>грн</sup></div>
+               </div>
+               <div class="price bonus">
+                   <div>Ціна за купоном</div>
+                   <div><span>${item.prise_coupon}</span><sup>грн</sup></div>
+               </div>
+           </div>
+
+           `
+       ))
+} else{
+   //вивід повідомлення про не знай 
    console.log('Блок товарів не знайдено')
 }
+
+//сортування масиву
+// itemsArray = itemsArray.sort()
+
+
+//Вивід знайдкного елементу
+//onsole.log(itemsDiv)
+
+//Перевірка існування знайденого блоку
+//if (itemsDiv) {
+   //Вивід знайденого елементу
+  // console.log(itemsDiv)
+
+   //Вивід значення поля знайденого елементу
+   // console.log('Поле classList:', itemsDiv.classList)
+   // console.log('Поле id:', itemsDiv.id)
+   // console.log('Поле clientWidth:', itemsDiv.clientWidth)
+   // console.log('Поле innerHTML:', itemsDiv.innerHTML)
+
+   //Додавання тексту в блок
+   // itemsDiv.innerHTML = 'Перший Програмно доданий текст'
+   // // itemsDiv.innerHTML = 'Другий Програмно доданий текст'
+   // itemsDiv.innerHTML += 'Другий Програмно доданий текст'
+   //Додавання відформатованого HTML коду в блок
+   // itemsDiv.innerHTML = '<div  class = item></div>'
+   // itemsDiv.innerHTML += '<div  class = "item"></div>'
+   // itemsDiv.innerHTML += '<div  class = "item"></div>'
+   // itemsDiv.innerHTML += '<div  class = "item"></div>'
+   // itemsDiv.innerHTML += '<div  class = "item"></div>'
+   //for(let i = 0; i < 100; i++) {
+      // itemsDiv.innerHTML += '<div class="item"></div>'
+   //}
+
+//} else {
+   //Вивід знайденого елементу
+   //console.log('Блок товарів не знайдено')
+//}
+
+// //виведення елементів відсортованого масиву
+// itemsArray.sort().forEach((item, index) =>{
+//     console.log(index + '-й елемент', item)
+// })
+
+//виведення елементів відсорт. масиву 
+// itemsArray.sort().forEach((item, index) =>{
+//     console.log(index + '-й елемент', item)
+// })
+
+//виведення в консоль масиву
+   // console.log(itemsArray)
+
+
+//вив. в консоль номерів та значень елементів масиву 
+// for(let i = 0; i < itemsArray.length; i++) {
+//          console.log( i + '-й елемент:', itemsArray[i])
+//      }
+
+
+//виведення в консоль масиву
+// console.log(itemsArray)
+// for(let i = 0; i < 100; i++) 
+//     itemsDiv.innerHTML += '<div class="item"></div>'
+//     console.log('item')
+//     }
